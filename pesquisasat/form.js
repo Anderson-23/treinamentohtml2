6259734f878e1662c2b7ac861781f9913c2232ce
Anderson-23 <MichelAnderson.Silva23@gmail.com>
2023-08-18 16:24:18 -0300

    UPAD

---
+++ pesquisasat/form.js
@@ -0,0 +1,26 @@
+const btn = document.querySelector("#igeral")
+const btn3 = document.querySelector("#iespera")
+const btn5= document.querySelector("#imed")
+const btn7 = document.querySelector("#iexame")
+const btn9 = document.querySelector("#iseg")
+const btn2 = document.querySelector("#irecepção")
+const btn4 = document.querySelector("#ienf")
+const btn6 = document.querySelector("#isocial")
+const btn8 = document.querySelector("#ilimpeza")
+const btn10 = document.querySelector("#iconforto")
+const form = document.querySelector("form")
+
+const formData = new FormData(form)
+
+btn.addEventListener("keypress", function(e){if(e.key === "Enter"){btn2.focus()}})
+btn2.addEventListener("keypress", function(e){if(e.key === "Enter"){btn3.focus()}})
+btn3.addEventListener("keypress", function(e){if(e.key === "Enter"){btn4.focus()}})
+btn4.addEventListener("keypress", function(e){if(e.key === "Enter"){btn5.focus()}})
+btn5.addEventListener("keypress", function(e){if(e.key === "Enter"){btn6.focus()}})
+btn6.addEventListener("keypress", function(e){if(e.key === "Enter"){btn7.focus()}})
+btn7.addEventListener("keypress", function(e){if(e.key === "Enter"){btn8.focus()}})
+btn8.addEventListener("keypress", function(e){if(e.key === "Enter"){btn9.focus()}})
+btn9.addEventListener("keypress", function(e){if(e.key === "Enter"){btn10.focus()}})
+// btn10.addEventListener("keypress", function(e){if(e.key === "Enter"){form}})
+
+console.log(formData);
