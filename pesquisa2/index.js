f57c2271c790b51a3835b93bad92e02b306380f2
Anderson-23 <MichelAnderson.Silva23@gmail.com>
2023-08-23 17:02:30 -0300

    update

---
+++ pesquisa2/index.js
@@ -0,0 +1,64 @@
+const rodarr = document.querySelectorAll(".rodar")
+const container = document.querySelector(".container")
+const container2 = document.getElementById("container")
+
+
+
+const containerb = document.querySelectorAll(".containerb")
+let currentItem = 0;
+const maxItem = containerb.length
+
+rodarr.forEach(rodarr =>{
+
+rodarr.addEventListener('click',()=>{
+const isleft = rodarr.classList.contains("esquerda");
+
+
+if(isleft){
+
+
+
+}
+
+
+console.log("clicou",isleft);
+
+
+
+
+})
+
+} )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// rodar.addEventListener("click", function(){
+
+//     idx++;
+ 
+// if(idx > containerb.length - 1){
+
+//     idx=0;
+
+// }
+
+// container2.style.transform = "rotateY(25deg)";
+
+// })
+
